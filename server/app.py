38eac5fceaab39057ad3c3e21473ba54511d7dc5
Akash NP <akashnp1998@gmail.com>
2023-11-11 14:57:22 +0400

    feat: implement endpoint to generate questions based on 'topic'
    process each topic request concurrently to improve performance.

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
+import asyncio
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+from ai_engine import chain
+from text_collector import get_material
+
 app = FastAPI()
 
-@app.get("/")
-def get_root():
-    return JSONResponse()
+@app.get("/practice/questions")
+async def get_root(topic: str):
+    material = get_material(topic, mock=True)
+    infos = []
+    for info in material:
+        content = info["title"]
+        content += "\n".join(i["content"] for i in info["children"])
+        infos.append(content)
+    result = await asyncio.gather(*[chain.ainvoke({"info": info}) for info in infos])
+    return JSONResponse(result)
