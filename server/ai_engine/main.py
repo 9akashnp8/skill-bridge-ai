c05c871f0989040983b4be732020fea22c8eb063
Akash NP <akashnp1998@gmail.com>
2023-11-12 10:32:32 +0400

    chore: add example question to question generation prompt
    ensure generated questions are scenario based

--- server/ai_engine/main.py
+++ server/ai_engine/main.py
@@ -10,18 +10,30 @@ You are an enthusiastic AI Agent who loves helping people. You
 are tasked with generating (python) practice questions to help improve
 ones knowledge.
 
-Based on solely the info provided below, generate good, real-world practice
-questions. The goal of these questions are to improve one knowledge
+Based on ONLY the "topic" and "info" provided below, generate good, real-world, scenario based
+practice questions. The goal of these questions are to improve one knowledge
 on the topic.
 
-{info}
+An example question:
+"Imagine you are developing a script to analyze server logs for a
+monitoring application. Each log entry includes a timestamp, log level, and a detailed message.
+However, you are specifically interested in extracting the timestamp and log level while
+excluding detailed module information to maintain brevity in your reports.
+
+Sample log: "2023-11-12 15:30:45 | ERROR | Critical issue occurred in ModuleX: Connection timeout"
+
+Create a function, extract_log_summary(log_entry, n), where log_entry is a string log entry,
+and n is a positive integer representing the number of characters to exclude from the end of the log entry."
+
+topic: {topic}
+info: {info}
 """
 prompt = PromptTemplate.from_template(prompt_template)
-prompt.format(info="THIS IS SOME INFO ABOUT SOMETHING")
+
 
 class CommaSeparatedListOutputParser(BaseOutputParser):
-
     def parse(self, text: str):
         return text.strip().split(", ")
 
-chain = prompt | llm | CommaSeparatedListOutputParser()
+
+chain = prompt | llm
--- server/app.py
+++ server/app.py
@@ -7,13 +7,11 @@ from text_collector import get_material
 
 app = FastAPI()
 
+
 @app.get("/practice/questions")
 async def get_root(topic: str):
     material = get_material(topic, mock=True)
-    infos = []
-    for info in material:
-        content = info["title"]
-        content += "\n".join(i["content"] for i in info["children"])
-        infos.append(content)
-    result = await asyncio.gather(*[chain.ainvoke({"info": info}) for info in infos])
+    result = await asyncio.gather(
+        *[chain.ainvoke({"topic": info.title, "info": info.info}) for info in material]
+    )
     return JSONResponse(result)
